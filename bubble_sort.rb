d2ac0a82bf8a826b8a140f00d9431cab8118dd18
franb312 <114260534+franb312@users.noreply.github.com>
2024-04-08 17:27:19 -0300

    Add files via upload
    rubocop correction

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 def bubble_sort(array)
-    n= array.length
-    loop do 
-        swap = false
-        (n-1).times do |i|
-            if array[i] > array[i+1]
-            array[i],array[i+1] = array[i+1], array[i]
-            swap = true
-            end
-        end
-        break unless swap
+  n = array.length
+  loop do
+    swap = false
+    (n - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swap = true
+      end
     end
-    array
+    break unless swap
+  end
+  array
 end
